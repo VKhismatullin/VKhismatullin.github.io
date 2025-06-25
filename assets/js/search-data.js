d552216b2a434071144aa730f73d75f0807a9646
VKhismatullin <vladimirkhismatullin@gmail.com>
2025-06-25 15:44:22 +0000

    Deploying to gh-pages from @ VKhismatullin/VKhismatullin.github.io@c3213d13e3554a6cb7b0cdc9e9d2813e9d0f123c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,18 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "post-a-post-with-jupyter-notebook",
-        
-          title: "a post with jupyter notebook",
-        
-        description: "an example of a blog post with jupyter notebook",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/jupyter-notebook/";
-          
-        },
-      },{id: "projects-chaos-and-decision-making",
+        },{id: "projects-chaos-and-decision-making",
           title: 'Chaos and decision-making',
           description: "Why beauty is in simplicity",
           section: "Projects",handler: () => {
